0083d6f64081c9b7da5f3d275e9af3cbb5af805f
Maya Posch <maya@nyanko.ws>
2019-10-15 23:00:42 +0200

    Replace atomic aliases with regular templated forms.

--- src/server/ffplay.h
+++ src/server/ffplay.h
@@ -17,7 +17,6 @@
 #include <iostream>
 #include <atomic>
 
-#include <vlc/vlc.h>
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_mutex.h>
 
@@ -38,27 +37,27 @@
 
 struct DataBuffer {
 	std::vector<std::string> data;	// The data byte, inside string instances.
-	std::atomic_uint64_t size;				// Number of bytes in data vector.
-	std::atomic_uint64_t currentIndex;		// The current index into the vector element.
-	std::atomic_uint32_t currentSlot;		// The current vector slot we're using.
-	std::atomic_uint32_t slotSize;			// Slot size in bytes.
-	std::atomic_uint32_t slotBytesLeft;		// Bytes left in the current slot.
-	std::atomic_uint32_t numSlots;			// Total number of slots in the data vector.
-	std::atomic_uint32_t nextSlot;			// Next slot to fill in the buffer vector.
-	std::atomic_uint32_t freeSlots;			// Slots free to write new data into.
-	std::atomic_uint32_t buffBytesLeft;		// Number of bytes available for reading in the buffer.
-	std::atomic_bool eof;					// Whether End of File for the source file has been reached.
+	std::atomic<uint64_t> size;				// Number of bytes in data vector.
+	std::atomic<uint64_t> currentIndex;		// The current index into the vector element.
+	std::atomic<uint32_t> currentSlot;		// The current vector slot we're using.
+	std::atomic<uint32_t> slotSize;			// Slot size in bytes.
+	std::atomic<uint32_t> slotBytesLeft;		// Bytes left in the current slot.
+	std::atomic<uint32_t> numSlots;			// Total number of slots in the data vector.
+	std::atomic<uint32_t> nextSlot;			// Next slot to fill in the buffer vector.
+	std::atomic<uint32_t> freeSlots;			// Slots free to write new data into.
+	std::atomic<uint32_t> buffBytesLeft;		// Number of bytes available for reading in the buffer.
+	std::atomic<bool> eof;					// Whether End of File for the source file has been reached.
 	
-	std::atomic_uint64_t buffIndexLow;		// File index at the buffer front.
-	std::atomic_uint64_t buffIndexHigh;		// File index at the buffer back.
+	std::atomic<uint64_t> buffIndexLow;		// File index at the buffer front.
+	std::atomic<uint64_t> buffIndexHigh;		// File index at the buffer back.
 	Poco::Mutex mutex;
 	Poco::Mutex dataMutex;
-	std::atomic_uint32_t activeSession;
+	std::atomic<uint32_t> activeSession;
 	Poco::Condition bufferDelayCondition;
 	Poco::Mutex bufferDelayMutex;
 	Poco::Condition requestCondition;
 	Poco::Mutex requestMutex;
-	std::atomic_bool requestInFlight;
+	std::atomic<bool> requestInFlight;
 };
 
 
