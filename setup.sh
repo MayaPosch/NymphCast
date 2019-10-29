d3b5116019b8a7b1e1afe19c7eb2794e660f7974
Maya Posch <maya@nyanko.ws>
2019-10-29 15:19:48 +0100

    Update setup.sh to build the entire server including NymphRPC.

--- setup.sh
+++ setup.sh
@@ -1,4 +1,51 @@
-#!/bin/sh
+#!/bin/bash
 
+# Installer script for the NymphCast server
+echo "UPDATE: $UPDATE"
+echo "PACKAGE: $PACKAGE"
 
-sudo apt install libsdl2-image-dev libsdl2-dev libpoco-dev libswscale libavcodec libavdevice libavformat libavutil libpostproc libswresample
+# Install the dependencies.
+sudo apt -y install libsdl2-image-dev libsdl2-dev libpoco-dev libswscale-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libpostproc-dev libswresample-dev
+
+if [ ! -z "${UPDATE}" ]; then
+	if [ -f "/usr/local/lib/libnymphrpc.a" ]; then
+		sudo rm /usr/local/lib/libnymphrpc.a
+		sudo rm -rf /usr/local/include/nymph
+	fi
+fi
+
+if [ -f "/usr/local/lib/libnymphrpc.a" ]; then
+	echo "NymphRPC library found in /usr/local/lib. Skipping installation."
+else
+	# Obtain current version of NymphRPC
+	git clone --depth 1 https://github.com/MayaPosch/NymphRPC.git
+	
+	# Build NymphRPC and install it.
+	echo "Installing NymphRPC..."
+	make -C NymphRPC/ lib
+	sudo mkdir -p /usr/local/include/nymph
+	sudo cp NymphRPC/src/*.h /usr/local/include/nymph/.
+	sudo chmod 766 /usr/local/include/nymph/*
+	sudo cp NymphRPC/lib/libnymphrpc.a /usr/local/lib/.
+	sudo chmod 766 /usr/local/lib/libnymphrpc.a
+fi
+
+# Remove NymphRPC folder.
+rm -rf NymphRPC
+
+# Build NymphCast server.
+if [ -f "src/server/bin/nymphcast_server" ]; then
+	rm src/server/bin/nymphcast_server
+fi
+
+make -C src/server/ clean
+make -C src/server/
+
+# Copy the wallpaper files into the bin folder.
+cp src/server/*.jpg src/server/bin/.
+
+if [ ! -z "${PACKAGE}" ]; then
+	# Package into a tar.gz
+	echo "Packaging into tar.gz file."
+	tar -czC src/server/bin -f nymphcast_server.tar.gz .
+fi
