f3c768f6441ad4cfd26038632663a2fc44803274
Maya Posch <maya@nyanko.ws>
2019-12-07 19:24:17 +0100

    Initial Raspbian card image wrangling script.

---
+++ tools/install_raspbian_img.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# install_raspbian_img.sh - Install cross-compiled NymphCast server on a fresh Raspbian .img file.
+
+IMG_FILE="2019-09-26-raspbian-buster.img"
+
+# Open img file.
+sudo kpartx -l $IMG_FILE
+sudo kpartx -av $IMG_FILE
+
+# Mount the partition.
+# First partition (loop0p1) is the boot partition, the second (loop0p2) is the system partition.
+sudo mount -o loop /dev/mapper/loop0p2 /mnt/ 
+
+# Copy files onto the partition.
+
+
+# Clean-up
+sudo umount /mnt/loop0p2
+sudo kpartx -d $IMG_FILE
