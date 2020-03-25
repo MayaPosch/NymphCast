570140f1d56f30949ac16ecdf79f74e81b4d13ff
Maya Posch <maya@nyanko.ws>
2020-03-25 09:02:11 +0100

    Change script to use the install routines in the server makefile.

--- install_linux.sh
+++ install_linux.sh
@@ -6,21 +6,24 @@
 TARGET_FOLDER="/opt/nymphcast"
 
 # Check for the presence of the target folder
-if [ -f "${TARGET_FOLDER}" ]; then
-	echo "Existing NymphCast folder found. Upgrading..."
-else
-	sudo mkdir -p "${TARGET_FOLDER}"
-fi
+# if [ -f "${TARGET_FOLDER}" ]; then
+	# echo "Existing NymphCast folder found. Upgrading..."
+# else
+	# sudo mkdir -p "${TARGET_FOLDER}"
+# fi
 
 # Copy the files to the target folder.
-sudo cp -r src/server/bin/* "${TARGET_FOLDER}"
+#sudo cp -r src/server/bin/* "${TARGET_FOLDER}"
+sudo make -C src/server/ install
 
 # Install systemd or openrc service.
 if [ -d "/run/systemd/system" ]; then
-	sudo cp src/server/systemd/nymphcast.service /etc/systemd/system/.
+	#sudo cp src/server/systemd/nymphcast.service /etc/systemd/system/.
+	sudo make -C src/server/ install-systemd
 	sudo ln -s /etc/systemd/system/nymphcast.service /etc/systemd/system/multi-user.target.wants/nymphcast.service
 else
-	sudo cp src/server/openrc/nymphcast /etc/init.d/nymphcast
+	#sudo cp src/server/openrc/nymphcast /etc/init.d/nymphcast
+	sudo make -C src/server/ install-openrc
 fi
 
 # Install Avahi service.
