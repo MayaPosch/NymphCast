d9a32521360153030f3805c12fe37056082dca3d
Maya Posch <maya@nyanko.ws>
2020-11-29 20:54:30 +0100

    Add new DataBuffer class.

---
+++ src/server/databuffer.cpp
@@ -0,0 +1,397 @@
+/*
+	databuffer.cpp - Implementation of the DataBufer class.
+	
+	Revision 0.
+	
+	Notes:
+			- 
+			
+	2020/11/19, Maya Posch
+*/
+
+
+#include "databuffer.h"
+
+#include <cstring>
+#include <chrono>
+
+
+// Static initialisations.
+uint8_t* DataBuffer::buffer = 0;
+uint8_t* DataBuffer::begin = 0;
+uint8_t* DataBuffer::end = 0;
+uint8_t* DataBuffer::front = 0;
+uint8_t* DataBuffer::back = 0;
+uint8_t* DataBuffer::index = 0;
+uint32_t DataBuffer::capacity = 0;
+uint32_t DataBuffer::size = 0;
+int64_t DataBuffer::filesize = 0;
+uint32_t DataBuffer::unread = 0;
+uint32_t DataBuffer::unreadLow = 0;
+uint32_t DataBuffer::unreadHigh = 0;
+uint32_t DataBuffer::bytesFreeLow = 0;
+uint32_t DataBuffer::bytesFreeHigh = 0;
+uint32_t DataBuffer::byteIndex = 0;
+uint32_t DataBuffer::byteIndexLow = 0;
+uint32_t DataBuffer::byteIndexHigh = 0;
+std::atomic<bool> DataBuffer::eof = false;
+std::atomic<DataBuffer::BufferState> DataBuffer::state;
+std::mutex DataBuffer::bufferMutex;
+//DataRequestCallback DataBuffer::dataRequestCallback = 0;
+SeekRequestCallback DataBuffer::seekRequestCallback = 0;
+std::condition_variable* DataBuffer::dataRequestCV = 0;
+std::mutex DataBuffer::dataWaitMutex;
+std::condition_variable DataBuffer::dataWaitCV;
+std::atomic<bool> DataBuffer::dataRequestPending = false;
+std::mutex DataBuffer::seekRequestMutex;
+std::condition_variable DataBuffer::seekRequestCV;
+std::atomic<bool> DataBuffer::seekRequestPending = false;
+uint32_t DataBuffer::sessionHandle = 0;
+
+std::mutex DataBuffer::streamTrackQueueMutex;
+std::queue<std::string> DataBuffer::streamTrackQueue;
+
+
+// --- INIT ---
+// Initialises new data buffer. Capacity is provided in bytes.
+// Returns false on error, otherwise true.
+bool DataBuffer::init(uint32_t capacity) {
+	bufferMutex.lock();
+	if (buffer != 0) {
+		// An existing buffer exists. Erase it first.
+		delete[] buffer;
+	}
+	
+	// Allocate new buffer and return result.
+	buffer = new uint8_t[capacity];
+	DataBuffer::capacity = capacity;
+	begin = buffer;
+	end = begin + capacity;
+	front = begin;
+	back = begin;
+	index = begin;
+	size = 0;
+	unread = 0;
+	unreadLow = 0;
+	unreadHigh = 0;
+	bytesFreeLow = 0;
+	bytesFreeHigh = capacity;
+	byteIndex = 0;
+	byteIndexLow = 0;
+	byteIndexHigh = 0;
+	state = DBS_IDLE;
+	bufferMutex.unlock();
+	
+	return true;
+}
+
+
+// --- CLEAN UP ---
+// Clean up resources, delete the buffer.
+bool DataBuffer::cleanup() {
+	if (buffer != 0) {
+		delete[] buffer;
+		buffer = 0;
+	}
+	
+	return true;
+}
+
+
+// --- SET DATA REQUEST CALLBACK ---
+/* void DataBuffer::setDataRequestCallback(DataRequestCallback callback) {
+	dataRequestCallback = callback;
+} */
+
+
+// --- SET SEEK REQUEST CALLBACK ---
+void DataBuffer::setSeekRequestCallback(SeekRequestCallback cb) {
+	seekRequestCallback = cb;
+}
+
+
+// --- SET DATA REQUEST CONDITION ---
+void DataBuffer::setDataRequestCondition(std::condition_variable* condition) {
+	dataRequestCV = condition;
+}
+
+
+// -- SET SESSION HANDLE ---
+void DataBuffer::setSessionHandle(uint32_t handle) {
+	sessionHandle = handle;
+}
+
+
+// --- GET SESSION HANDLE ---
+uint32_t DataBuffer::getSessionHandle() {
+	return sessionHandle;
+}
+
+
+// --- SET FILE SIZE ---
+void DataBuffer::setFileSize(int64_t size) {
+	filesize = size;
+}
+
+
+// --- GET FILE SIZE ---
+int64_t DataBuffer::getFileSize() {
+	return filesize;
+}
+
+
+// --- START ---
+// Starts calling the data request handler to obtain data.
+bool DataBuffer::start() {
+	if (dataRequestCV == 0) { return false; }
+	
+	dataRequestPending = true;
+	dataRequestCV->notify_one();
+	
+	return true;
+}
+
+
+// --- REQUEST DATA ---
+void DataBuffer::requestData() {
+	if (dataRequestCV == 0) { return; }
+	
+	// Trigger a data request from the client.
+	dataRequestPending = true;
+	dataRequestCV->notify_one();
+	
+	// Wait until we have received data or time out.
+	std::unique_lock<std::mutex> lk(dataWaitMutex);
+	using namespace std::chrono_literals;
+	while (dataWaitCV.wait_for(lk, 150ms) != std::cv_status::timeout) {
+		//if (stat == std::cv_status::timeout) { break; }
+		if (!dataRequestPending) { break; }
+	}
+}
+
+
+// --- RESET ---
+// Reset the buffer to the initialised state. This leaves the existing allocated buffer intact, 
+// but erases its contents.
+bool DataBuffer::reset() {
+	front = begin;
+	back = begin;
+	size = 0;
+	index = begin;
+	unread = 0;
+	unreadLow = 0;
+	unreadHigh = 0;
+	bytesFreeLow = 0;
+	bytesFreeHigh = capacity;
+	byteIndex = 0;
+	byteIndexLow = 0;
+	byteIndexHigh = 0;
+	eof = false;
+	state = DBS_IDLE;
+	dataRequestPending = false;
+	seekRequestPending = false;
+	
+	return true;
+}
+
+
+// --- SEEK ---
+// Seek to a specific point in the data.
+// Returns the new absolute byte position in the file, or -1 in case of failure.
+int64_t DataBuffer::seek(DataBufferSeek mode, int64_t offset) {
+	// Calculate absolute byte index.
+	int64_t new_offset = -1;
+	if 		(mode == DB_SEEK_START)		{ new_offset = offset; }
+	else if (mode == DB_SEEK_CURRENT) 	{ new_offset = byteIndex + offset; }
+	else if (mode == DB_SEEK_END)		{ new_offset = filesize - offset; }
+	
+	// Check whether we have the requested data in the buffer.
+	if (new_offset < byteIndexLow || new_offset > byteIndexHigh) {
+		// Data is not in buffer. Reset buffer and send seek request to client.
+		reset();
+		byteIndexLow = new_offset;
+		byteIndexHigh = new_offset;
+		if (seekRequestCallback == 0) { return -1; }
+		seekRequestPending = true;
+		state = DBS_SEEKING;
+		seekRequestCallback(sessionHandle, new_offset);
+		
+		// Wait for response.
+		std::unique_lock<std::mutex> lk(seekRequestMutex);
+		using namespace std::chrono_literals;
+		while (!seekRequestPending) {
+			std::cv_status stat = seekRequestCV.wait_for(lk, 150ms);
+			if (stat == std::cv_status::timeout) { return -1; }
+			//if (!seekRequestPending) { break; }
+		}
+		
+		state = DBS_IDLE;
+	}
+	else {
+		// Set the new position in the buffer.
+		uint32_t oldUnread = unread;
+		byteIndex = new_offset;							// Absolute byte index.
+		index = front + (new_offset - byteIndexLow);	// Index into buffer translation.
+		unread = byteIndexHigh - new_offset;
+		unreadLow += unread - oldUnread;
+		unreadHigh -= unread - oldUnread;
+	}
+	
+	return new_offset;
+}
+
+
+// --- SEEKING ---
+bool DataBuffer::seeking() {
+	return (state == DBS_SEEKING);
+}
+
+
+// --- READ ---
+// Try to read 'len' bytes from the buffer, into the provided buffer.
+// Returns the number of bytes read, or 0 in case of an error.
+uint32_t DataBuffer::read(uint32_t len, uint8_t* bytes) {
+	if (!eof && len < unread) {
+		// More data should be available on the client, try to request it.
+		requestData();
+	}
+	
+	bufferMutex.lock();
+	uint32_t bytesRead = 0;
+	if (len <= unreadHigh) {
+		// Read requested data in one chunk from the buffer back.
+		memcpy(bytes, index, len);
+		
+		// Update counters, etc.
+		index += len;
+		unreadHigh -= len;
+		bytesRead += len;
+		if (unreadHigh == 0) { index = begin; } // Read from the front. Back is exhausted.
+	}
+	else {
+		// Read the remaining unread bytes from the back, then continue reading from the front.
+		memcpy(bytes, index, unreadHigh);
+		index = begin;
+		unreadHigh = 0;
+		uint32_t bytesToRead = len - bytesRead;
+		if (bytesToRead <= unreadLow) {
+			// Read the remaining requested bytes in one chunk.
+			memcpy(bytes + bytesRead, index, bytesToRead);
+			index += bytesToRead;
+			bytesRead += bytesToRead;
+		}
+		else {
+			// Not enough bytes left in the buffer. Read what we can, then return.
+			memcpy(bytes + bytesRead, index, unreadLow);
+			index += unreadLow;
+			bytesRead += unreadLow;
+		}
+	}
+	
+	// Update counters.
+	unread -= bytesRead;
+	byteIndex += bytesRead;
+	
+	bufferMutex.unlock();
+	
+	return bytesRead;
+}
+
+
+// --- WRITE ---
+// Write data into the buffer.
+uint32_t DataBuffer::write(std::string &data) {
+	// First check whether we can perform a straight copy. For this we need enough available bytes
+	// at the end of the buffer. Else we have to attempt to write the remainder into the front of
+	// the buffer.
+	bufferMutex.lock();
+	dataRequestPending = false;
+	uint32_t bytesWritten = 0;
+	if (data.length() <= bytesFreeHigh) {
+		// Copy the data into the buffer.
+		memcpy(back, data.data(), data.length());
+		bytesWritten = data.length();
+		bytesFreeHigh -= data.length();
+		unreadHigh += data.length();
+		
+		// Move back pointer to just beyond the newly written data.
+		back += bytesWritten;
+		if (bytesFreeHigh == 0) {
+			back = begin;
+		}
+	}
+	else {
+		// Copy what we can, then try to copy the rest into the beginning of the buffer
+		memcpy(back, data.data(), bytesFreeHigh);
+		bytesWritten = bytesFreeHigh;
+		back = begin; // Buffer loops back to the front after it fills up.
+		
+		uint32_t bytesToWrite = data.length() - bytesFreeHigh;
+		bytesFreeHigh = 0;
+		unreadHigh += data.length();
+		if (bytesToWrite > bytesFreeLow) {
+			// Buffer is too small for remaining bytes. Copy what we can.
+			memcpy(back, data.data() + bytesWritten, bytesFreeLow);
+			bytesWritten += bytesFreeLow;
+			unreadLow += bytesFreeLow;
+			bytesFreeLow = 0;
+		}
+		else {
+			// Straight copy into buffer.
+			memcpy(back, data.data() + bytesWritten, bytesToWrite);
+			bytesWritten += bytesToWrite;
+			unreadLow += bytesToWrite;
+			bytesFreeLow -= bytesToWrite;
+		}
+	}
+		
+	// Update counters.
+	size += bytesWritten;
+	unread += bytesWritten;
+	byteIndexHigh += bytesWritten;
+	
+	bufferMutex.unlock();
+	
+	// If we're in seeking mode, signal that we're done.
+	if (state == DBS_SEEKING) {
+		seekRequestPending = false;
+		seekRequestCV.notify_one();
+	}
+	
+	return bytesWritten;
+}
+
+
+// --- SET EOF ---
+// Set the End-Of-File status of the file being streamed.
+void DataBuffer::setEof(bool eof) {
+	DataBuffer::eof = eof;
+}
+
+
+// --- ADD STREAM TRACK ---
+// Add a streaming track to the queue.
+void DataBuffer::addStreamTrack(std::string track) {
+	streamTrackQueueMutex.lock();
+	streamTrackQueue.push(track);
+	streamTrackQueueMutex.unlock();
+}
+
+
+// --- HAS STREAM TRACK ---
+bool DataBuffer::hasStreamTrack() {
+	return !streamTrackQueue.empty();
+}
+
+
+// --- GET STREAM TRACK ---
+// Returns the next stream string in the queue, or an empty string if queue is empty.
+std::string DataBuffer::getStreamTrack() {
+	streamTrackQueueMutex.lock();
+	if (streamTrackQueue.empty()) { return std::string(); }
+	std::string tStr = streamTrackQueue.front();
+	streamTrackQueue.pop();
+	streamTrackQueueMutex.unlock();
+	
+	return tStr;
+}
---
+++ src/server/databuffer.h
@@ -0,0 +1,94 @@
+/*
+	databuffer.h - Data Buffer header.
+	
+	Revision 0
+	
+	Features:
+			- Provides API for a ring buffer implementation.
+			
+	2020/11/19, Maya Posch
+*/
+
+
+#include <atomic>
+#include <mutex>
+#include <functional>
+#include <condition_variable>
+#include <queue>
+#include <string>
+
+
+//typedef std::function<bool()> DataRequestCallback;
+typedef std::function<void(uint32_t, int64_t)> SeekRequestCallback;
+
+enum DataBufferSeek {
+	DB_SEEK_START = 0,
+	DB_SEEK_CURRENT,
+	DB_SEEK_END
+};
+
+
+class DataBuffer {
+	enum BufferState {
+		DBS_IDLE = 0,
+		DBS_BUFFERING,
+		DBS_SEEKING
+	};
+	
+	static uint8_t* buffer;		// Pointer to buffer.
+	static uint8_t* begin;		// Pointer to buffer start (idx 0).
+	static uint8_t* end;		// Pointer to buffer end (idx Nsize).
+	static uint8_t* front;		// Pointer to front of data in buffer (low).
+	static uint8_t* back;		// Pointer to back of data in buffer (last byte + 1).
+	static uint8_t* index;		// Pointer to first unread byte or buffer start.
+	static uint32_t capacity;	// Total capacity of buffer in bytes.
+	static uint32_t size;		// Total size of data in buffer in bytes.
+	static int64_t filesize;	// Size of the media file being streamed, in bytes.
+	static uint32_t unread;		// Number of unread bytes in the buffer.
+	static uint32_t unreadLow;		// Number of unread bytes in the buffer at the front (low).
+	static uint32_t unreadHigh;		// Number of unread bytes in the buffer at the back (high).
+	static uint32_t bytesFreeLow;	// Number of free bytes in buffer at the front (low).
+	static uint32_t bytesFreeHigh;	// Number of free bytes in buffer at the back (high).
+	static uint32_t byteIndex;		// First unread byte index into the media file data.
+	static uint32_t byteIndexLow;	// Lowest media file byte index present in the buffer.
+	static uint32_t byteIndexHigh;	// Highest media file byte index present in the buffer.
+	static std::mutex bufferMutex;
+	static std::atomic<bool> eof;
+	static std::atomic<BufferState> state;
+	//static DataRequestCallback dataRequestCallback = 0;
+	static SeekRequestCallback seekRequestCallback;
+	static std::condition_variable* dataRequestCV;
+	static std::mutex dataWaitMutex;
+	static std::condition_variable dataWaitCV;
+	static std::atomic<bool> dataRequestPending;
+	static std::mutex seekRequestMutex;
+	static std::condition_variable seekRequestCV;
+	static std::atomic<bool> seekRequestPending;
+	static uint32_t sessionHandle;		// Active session this buffer is associated with.
+	
+	static std::mutex streamTrackQueueMutex;
+	static std::queue<std::string> streamTrackQueue;
+	
+public:
+	static bool init(uint32_t capacity);
+	static bool cleanup();
+	//static void setDataRequestCallback(DataRequestCallback callback);
+	static void setSeekRequestCallback(SeekRequestCallback cb);
+	static void setDataRequestCondition(std::condition_variable* condition);
+	static void setSessionHandle(uint32_t handle);
+	static uint32_t getSessionHandle();
+	static void setFileSize(int64_t size);
+	static int64_t getFileSize();
+	static bool start();
+	static void requestData();
+	static bool reset();
+	static int64_t seek(DataBufferSeek mode, int64_t offset);
+	static bool seeking();
+	static uint32_t read(uint32_t len, uint8_t* bytes);
+	static uint32_t write(std::string &data);
+	static void setEof(bool eof);
+	
+	static void addStreamTrack(std::string track);
+	static bool hasStreamTrack();
+	static std::string getStreamTrack();
+};
