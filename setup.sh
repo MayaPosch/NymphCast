b6b116ebf859fa508135f8376a3a073a73fe58c1
Maya Posch <maya@nyanko.ws>
2021-09-01 20:31:41 +0200

    Add MSYS2 support to installing the dependencies.

--- setup.sh
+++ setup.sh
@@ -5,20 +5,48 @@ echo "UPDATE: $UPDATE"
 echo "PACKAGE: $PACKAGE"
 
 # Install the dependencies.
-if [ -x "$(command -v brew)" ]; then
-	brew update
-	brew install sdl2 sdl2_image poco ffmpeg freetype freeimage rapidjson pkg-config curl 
-	brew install --cask vlc
-elif [ -x "$(command -v apt)" ]; then
-	sudo apt update
-	sudo apt -y install git g++ libsdl2-image-dev libsdl2-dev libpoco-dev libswscale-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libpostproc-dev libswresample-dev pkg-config libfreetype6-dev libfreeimage-dev rapidjson-dev libcurl4-gnutls-dev libvlc-dev
-elif [ -x "$(command -v apk)" ]; then
-	sudo apk update
-	sudo apk add poco-dev sdl2-dev sdl2_image-dev ffmpeg-dev openssl-dev freetype-dev freeimage-dev rapidjson-dev alsa-lib-dev glew-dev nymphrpc-dev curl-dev vlc-dev
-elif [ -x "$(command -v pacman)" ]; then
-	sudo pacman -Syy 
-	sudo pacman -S --noconfirm --needed sdl2 sdl2_image poco ffmpeg freetype2 freeimage rapidjson pkgconf curl vlc
-fi
+case "$(uname -s)" in
+	Darwin)
+		echo 'Mac OS X'
+		if [ -x "$(command -v brew)" ]; then
+			brew update
+			brew install sdl2 sdl2_image poco ffmpeg freetype freeimage rapidjson pkg-config curl 
+			brew install --cask vlc
+		fi
+		;;
+
+	Linux)
+		echo 'Linux'
+		if [ -x "$(command -v apt)" ]; then
+			sudo apt update
+			sudo apt -y install git g++ libsdl2-image-dev libsdl2-dev libpoco-dev libswscale-dev libavcodec-dev libavdevice-dev libavformat-dev libavutil-dev libpostproc-dev libswresample-dev pkg-config libfreetype6-dev libfreeimage-dev rapidjson-dev libcurl4-gnutls-dev libvlc-dev
+		elif [ -x "$(command -v apk)" ]; then
+			sudo apk update
+			sudo apk add poco-dev sdl2-dev sdl2_image-dev ffmpeg-dev openssl-dev freetype-dev freeimage-dev rapidjson-dev alsa-lib-dev glew-dev nymphrpc-dev curl-dev vlc-dev
+		elif [ -x "$(command -v pacman)" ]; then
+			sudo pacman -Syy 
+			sudo pacman -S --noconfirm --needed sdl2 sdl2_image poco ffmpeg freetype2 freeimage rapidjson pkgconf curl vlc
+		fi
+		;;
+
+	CYGWIN*|MINGW32*|MSYS*|MINGW*)
+		echo 'MS Windows/MinGW'
+		if [ -x "$(command -v pacman)" ]; then
+			pacman -Syy 
+			pacman -S --noconfirm --needed mingw-w64-x86_64-SDL2 mingw-w64-x86_64-SDL2_image mingw-w64-x86_64-poco mingw-w64-x86_64-ffmpeg mingw-w64-x86_64-freetype mingw-w64-x86_64-freeimage mingw-w64-x86_64-rapidjson pkgconf curl mingw-w64-x86_64-vlc
+		fi
+		
+		# Bail out here for now until MSYS2 support is implemented for the rest.
+		echo 'Install libnymphrpc & libnymphcast before building server.'
+		exit
+		;;
+
+	*)
+		echo 'Unsupported OS'
+		exit
+		;;
+esac
+
 
 if [ ! -z "${UPDATE}" ]; then
 	if [ -f "/usr/local/lib/libnymphrpc.a" ]; then
