69559f8d5518f475ec4c3d224f753676e54cece3
Maya Posch <maya@nyanko.ws>
2022-01-10 15:59:19 +0100

    Increase data request timeout to 500 ms after trials with RPi 2B/WiFi.

--- src/server/databuffer.cpp
+++ src/server/databuffer.cpp
@@ -187,13 +187,13 @@ void DataBuffer::requestData() {
 	// Wait until we have received data or time out.
 	std::unique_lock<std::mutex> lk(dataWaitMutex);
 	using namespace std::chrono_literals;
-	uint8_t timeout = 500;
+	uint32_t timeout = 5000;
 	while (1) { 
 		dataWaitCV.wait_for(lk, 100us);
 		if (!dataRequestPending) { break; }
 		if (--timeout == 0) {
 #ifdef DEBUG
-			std::cerr << "RequestData timeout after 50 ms." << std::endl;
+			std::cerr << "RequestData timeout after 500 ms." << std::endl;
 #endif
 			break;
 		}
