b4b65c34ed5569fd54e65467f415fb3bade1a1d1
Maya Posch <maya@nyanko.ws>
2022-02-07 15:14:10 +0100

    Refactor LCDMutex to replace pthread with std::thread.

--- src/server/lcdapi/api/LCDMutex.h
+++ src/server/lcdapi/api/LCDMutex.h
@@ -1,68 +1,72 @@
 /* Copyright 2012-2017 Simon Dawson <spdawson@gmail.com>
 
-   This file is part of lcdapi.
+	 This file is part of lcdapi.
 
-   lcdapi is free software: you can redistribute it and/or modify
-   it under the terms of the GNU Lesser General Public License as
-   published by the Free Software Foundation, either version 3 of
-   the License, or (at your option) any later version.
+	 lcdapi is free software: you can redistribute it and/or modify
+	 it under the terms of the GNU Lesser General Public License as
+	 published by the Free Software Foundation, either version 3 of
+	 the License, or (at your option) any later version.
 
-   lcdapi is distributed in the hope that it will be useful,
-   but WITHOUT ANY WARRANTY; without even the implied warranty of
-   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-   GNU Lesser General Public License for more details.
+	 lcdapi is distributed in the hope that it will be useful,
+	 but WITHOUT ANY WARRANTY; without even the implied warranty of
+	 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.	See the
+	 GNU Lesser General Public License for more details.
 
-   You should have received a copy of the GNU Lesser General Public
-   License along with lcdapi.  If not,
-   see <http://www.gnu.org/licenses/>. */
+	 You should have received a copy of the GNU Lesser General Public
+	 License along with lcdapi.	If not,
+	 see <http://www.gnu.org/licenses/>. 
+	 
+	2022/02/07, Maya Posch.
+*/
 
 #ifndef _LCDAPI_API_LCDMUTEX_H_
 #define _LCDAPI_API_LCDMUTEX_H_
 
-#include <pthread.h>
+#include <thread>
+#include <mutex>
 
 namespace lcdapi {
 
 /** \class LCDMutex LCDMutex.h "api/LCDMutex.h"
- *  \brief A class implementing mutexes.
- *  \ingroup main
- *  This class is used to create some recursive mutexes.
- *   If a thread has already locked it, it won't be blocked by
- *   another lock.
+ *	\brief A class implementing mutexes.
+ *	\ingroup main
+ *	This class is used to create some recursive mutexes.
+ *	 If a thread has already locked it, it won't be blocked by
+ *	 another lock.
  */
 class LCDMutex {
  private:
-  ::pthread_mutex_t _mutex;
-  ::pthread_t _owner;
+	std::mutex _mutex;
+	std::thread::id _owner;
  public:
-  /**
-   * \brief Default constructor.
-   *
-   * This constructor initializes the mutex.
-   */
-  LCDMutex();
+	/**
+	 * \brief Default constructor.
+	 *
+	 * This constructor initializes the mutex.
+	 */
+	LCDMutex();
 
-  /**
-   * \brief Destructor.
-   *
-   * This destructor destroys he mutex.
-   */
-  ~LCDMutex();
+	/**
+	 * \brief Destructor.
+	 *
+	 * This destructor destroys he mutex.
+	 */
+	~LCDMutex();
 
-  /**
-   * \brief Lock the mutex.
-   *
-   * This method is used to lock the mutex. It will have an effect
-   *  only if it is not already locked by the same thread.
-   */
-  void lock();
+	/**
+	 * \brief Lock the mutex.
+	 *
+	 * This method is used to lock the mutex. It will have an effect
+	 *	only if it is not already locked by the same thread.
+	 */
+	void lock();
 
-  /**
-   * \brief Unlock the mutex.
-   *
-   * This method will release the mutex so another thread can acquire it.
-   */
-  void unlock();
+	/**
+	 * \brief Unlock the mutex.
+	 *
+	 * This method will release the mutex so another thread can acquire it.
+	 */
+	void unlock();
 };
 
 } // end of lcdapi namespace
