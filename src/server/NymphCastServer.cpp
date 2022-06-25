d99d57d462a54cbab37cd56f505e904cac41c926
Maya Posch <maya@nyanko.ws>
2022-06-25 11:56:10 +0200

    Redirect stdout, stderr on Android.

--- src/server/NymphCastServer.cpp
+++ src/server/NymphCastServer.cpp
@@ -80,6 +80,44 @@ using namespace Poco;
 
 #ifdef __ANDROID__
 #include <android_native_app_glue.h>
+
+// Set up native logging by redirecting stdout to Android logging.
+static int pfd[2];
+static pthread_t thr;
+static const char* tag = "NymphCastServer";
+
+#include <android/log.h>
+
+static void* thread_func(void*) {
+    ssize_t rdsz;
+    char buf[128];
+    while ((rdsz = read(pfd[0], buf, sizeof buf - 1)) > 0) {
+        //if (buf[rdsz - 1] == '\n') --rdsz; // Remove newline if it exists.
+        buf[rdsz] = 0;  // add null-terminator
+        __android_log_write(ANDROID_LOG_DEBUG, tag, buf);
+    }
+    
+    return 0;
+}
+
+
+int start_logger(const char* app_name) {
+    tag = app_name;
+
+    /* make stdout line-buffered and stderr unbuffered */
+    setvbuf(stdout, 0, _IOLBF, 0);
+    setvbuf(stderr, 0, _IONBF, 0);
+
+    /* create the pipe and redirect stdout and stderr */
+    pipe(pfd);
+    dup2(pfd[1], 1);
+    dup2(pfd[1], 2);
+
+    /* spawn the logging thread */
+    if (pthread_create(&thr, 0, thread_func, 0) == -1) { return -1; }    
+    pthread_detach(thr);
+    return 0;
+}
 #endif
 
 
