63d37a2dc7d4edbfe7c8b00f61deddad5c21c97b
Maya Posch <mayaposch@gmail.com>
2023-04-03 22:28:02 +0200

    Replace deprecated avutil call with av_calloc.

--- src/server/ffplay/stream_handler.cpp
+++ src/server/ffplay/stream_handler.cpp
@@ -137,7 +137,8 @@ AVDictionary **setup_find_stream_info_opts(AVFormatContext *s, AVDictionary *cod
     int i;
     AVDictionary **opts;
     if (!s->nb_streams) { return NULL; }
-	opts = (AVDictionary**) av_mallocz_array(s->nb_streams, sizeof(*opts));
+	//opts = (AVDictionary**) av_mallocz_array(s->nb_streams, sizeof(*opts));
+	opts = (AVDictionary**) av_calloc(s->nb_streams, sizeof(*opts));
     if (!opts) {
         av_log(NULL, AV_LOG_ERROR, "Could not alloc memory for stream options.\n");
         return NULL;
