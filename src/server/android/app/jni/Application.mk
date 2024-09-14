63816a486dadca7de1567b3abd1cf6e68330b8e5
Maya Posch <1096451+MayaPosch@users.noreply.github.com>
2024-09-14 15:42:24 +0200

    Update Android build files for NCS.

--- src/server/android/app/jni/Application.mk
+++ src/server/android/app/jni/Application.mk
@@ -4,12 +4,13 @@
 # https://developer.android.com/ndk/guides/cpp-support
 APP_STL := c++_shared
 
-APP_ABI := arm64-v8a x86_64
+APP_ABI := arm64-v8a x86_64 
+#armeabi-v7a
 
 # Min runtime API level
 APP_PLATFORM=android-24
 
-#APP_CFLAGS := -fsanitize=address -fno-omit-frame-pointer
-#APP_LDFLAGS := -fsanitize=address
+APP_CFLAGS := -fsanitize=address -fno-omit-frame-pointer
+APP_LDFLAGS := -fsanitize=address
 
 #APP_SHORT_COMMANDS := true
